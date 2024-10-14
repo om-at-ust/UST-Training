f7f6ddc9edd186280c2ce8b7c5745f7e3d400c47
om-at-ust <omdubey737683r@gmail.com>
2024-10-14 17:34:26 +0530

    JWT Validation done

--- SpringsecurityV3/src/main/java/com/UST/SpringsecurityV3/controller/UserController.java
+++ SpringsecurityV3/src/main/java/com/UST/SpringsecurityV3/controller/UserController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,6 +39,11 @@ public class UserController {
     @GetMapping("/getAllUsers")
     public List<UserInfo> getAllUsers(){
         return userService.getAllUsers();
+    }
+    @GetMapping("/validate/token")
+    public String validateToken(@RequestParam String Token){
+        jwtService.validateToken(Token);
+        return "Token validated successfully";
     }
 
 
